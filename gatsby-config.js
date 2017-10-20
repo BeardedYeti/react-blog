fda1905cf1f90825bf6534b0f9c087a3eb3c01a1
Colin Smith <cmsgoboston41@gmail.com>
2017-10-20 11:20:38 -0400

    Add gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,12 @@ module.exports = {
   plugins: [
   	`gatsby-plugin-catch-links`,
   	`gatsby-plugin-react-helmet`,
+  	{
+  		resolve: `gatsby-source-filesystem`,
+  		options: {
+  			path: `${__dirname}/src/pages`,
+  			name: 'pages',
+  		},
+  	}
   ],
 }
